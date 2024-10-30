4d5f9f191f97e678e955e034d5ad359185971691
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2024-10-30 20:03:53 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+
+
+def show_weather(places):
+    url_template = 'https://wttr.in/{}'
+    payload = {
+        'lang': 'ru',
+        'TnMq': '',
+    }
+    for place in places:
+        response = requests.get(url_template.format(place), params=payload)
+        response.raise_for_status()
+        print(response.text)
+
+
+def main():
+    places = ('Лондон', 'SVO', 'Череповец')
+    show_weather(places)
+
+
+if __name__ == '__main__':
+    main()
